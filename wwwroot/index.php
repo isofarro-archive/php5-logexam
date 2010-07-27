f43c304dbf04c48c3a09f781691ab489054e67cc
Isofarro <isofarro2@gmail.com>
2010-07-27 20:20:47 +0100

    Skeleton webapp structure

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "<h1>Logexam</h1>";
+$path = $_SERVER['REQUEST_URI'];
+$segments = explode('/', trim($path, '/'));
+
+$dataset = $segments[0];
+echo "<h1>Logexam: {$dataset}</h1>";
+//echo '<pre>'; print_r($segments); echo '</pre>';
 
 ?>
